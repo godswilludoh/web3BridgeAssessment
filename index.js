72dc87db06998a0fd81ad3ffe2e4e14f1b1efed0
Godswill Udoh <godswilludoh@gmail.com>
2022-05-27 21:46:31 +0100

    added a clear interval function to the different iterations and made changes to the leaderboard function

--- index.js
+++ index.js
@@ -14,46 +14,55 @@ const participants = [
 		id: 1,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 2,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 3,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 4,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 5,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 6,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 7,
 		startTime: '',
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 8,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 9,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 10,
@@ -63,11 +72,13 @@ const participants = [
 		id: 11,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 12,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 13,
@@ -78,22 +89,28 @@ const participants = [
 		id: 14,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 	{
 		id: 15,
 		startTime: 0,
 		iterationCount: 0,
+		name: '',
 	},
 ];
 
 startIteration1.addEventListener('click', () => {
 	const id = document.getElementById('iteration1').value;
 	participants[id - 1].startTime = new Date();
-	let time = 3600;
-	setInterval(() => {
+	participants[id - 1].name = document.getElementById('name1').value;
+	let time = 30;
+	const setTime = setInterval(() => {
 		time -= 1;
 		document.getElementById('count1').innerText = time;
 	}, 1000);
+	setTimeout(() => {
+		clearInterval(setTime);
+	}, 30000);
 });
 
 finishIteration1.addEventListener('click', () => {
@@ -101,7 +118,7 @@ finishIteration1.addEventListener('click', () => {
 	const seconds = participants[id - 1].startTime.getTime() / 1000;
 	const currentTime = new Date().getTime() / 1000;
 	console.log(id, seconds);
-	if (currentTime >= seconds + 3600) {
+	if (currentTime >= seconds + 30) {
 		participants[id - 1].iterationCount = 1;
 		resetLeaderBoard(id);
 	} else {
@@ -113,12 +130,17 @@ startIteration2.addEventListener('click', () => {
 	const id = document.getElementById('iteration2').value;
 	if (participants[id - 1].iterationCount !== 1) {
 		alert('you have to finish iteration 1');
-	} else participants[id - 1].startTime = new Date();
-	let time = 3600;
-	setInterval(() => {
-		time -= 1;
-		document.getElementById('count2').innerText = time;
-	}, 1000);
+	} else {
+		participants[id - 1].startTime = new Date();
+		let time = 30;
+		const setTime = setInterval(() => {
+			time -= 1;
+			document.getElementById('count2').innerText = time;
+		}, 1000);
+		setTimeout(() => {
+			clearInterval(setTime);
+		}, 30000);
+	}
 });
 
 finishIteration2.addEventListener('click', () => {
@@ -127,7 +149,7 @@ finishIteration2.addEventListener('click', () => {
 	console.log(seconds);
 	const currentTime = new Date().getTime() / 1000;
 	if (participants[id - 1].iterationCount === 1) {
-		if (currentTime >= seconds + 3600) {
+		if (currentTime >= seconds + 30) {
 			participants[id - 1].iterationCount = 2;
 			resetLeaderBoard(id);
 		} else {
@@ -139,13 +161,18 @@ finishIteration2.addEventListener('click', () => {
 startIteration3.addEventListener('click', () => {
 	const id = document.getElementById('iteration3').value;
 	if (participants[id - 1].iterationCount !== 2) {
-		console.log('you have to finish iteration 2');
-	} else participants[id - 1].startTime = new Date();
-	let time = 3600;
-	setInterval(() => {
-		time -= 1;
-		document.getElementById('count3').innerText = time;
-	}, 1000);
+		alert('you have to finish iteration 2');
+	} else {
+		participants[id - 1].startTime = new Date();
+		let time = 30;
+		const setTime = setInterval(() => {
+			time -= 1;
+			document.getElementById('count3').innerText = time;
+		}, 1000);
+		setTimeout(() => {
+			clearInterval(setTime);
+		}, 30000);
+	}
 });
 
 finishIteration3.addEventListener('click', () => {
@@ -153,7 +180,7 @@ finishIteration3.addEventListener('click', () => {
 	const seconds = participants[id - 1].startTime.getTime() / 1000;
 	const currentTime = new Date().getTime() / 1000;
 	if (participants[id - 1].iterationCount === 2) {
-		if (currentTime >= seconds + 3600) {
+		if (currentTime >= seconds + 30) {
 			participants[id - 1].iterationCount = 3;
 			resetLeaderBoard(id);
 		} else {
@@ -165,13 +192,18 @@ finishIteration3.addEventListener('click', () => {
 startIteration4.addEventListener('click', () => {
 	const id = document.getElementById('iteration4').value;
 	if (participants[id - 1].iterationCount !== 3) {
-		console.log('you have to finish iteration 3');
-	} else participants[id - 1].startTime = new Date();
-	let time = 3600;
-	setInterval(() => {
-		time -= 1;
-		document.getElementById('count4').innerText = time;
-	}, 1000);
+		alert('you have to finish iteration 3');
+	} else {
+		participants[id - 1].startTime = new Date();
+		let time = 30;
+		const setTime = setInterval(() => {
+			time -= 1;
+			document.getElementById('count4').innerText = time;
+		}, 1000);
+		setTimeout(() => {
+			clearInterval(setTime);
+		}, 30000);
+	}
 });
 
 finishIteration4.addEventListener('click', () => {
@@ -179,7 +211,7 @@ finishIteration4.addEventListener('click', () => {
 	const seconds = participants[id - 1].startTime.getTime() / 1000;
 	const currentTime = new Date().getTime() / 1000;
 	if (participants[id - 1].iterationCount === 3) {
-		if (currentTime >= seconds + 3600) {
+		if (currentTime >= seconds + 30) {
 			participants[id - 1].iterationCount = 4;
 			resetLeaderBoard(id);
 		} else {
@@ -190,32 +222,52 @@ finishIteration4.addEventListener('click', () => {
 
 const resetLeaderBoard = (id) => {
 	const part = participants[id - 1];
-	const index = leaderBoard.findIndex((item) => item.id === id);
+	const index = leaderBoard.findIndex((item) => item.id === part.id);
 	console.log(index);
-	if (index === undefined) leaderBoard.push(part);
+	if (index === -1) leaderBoard.push(part);
 	else leaderBoard.splice(index, 1, part);
 	leaderBoard.sort(function (a, b) {
 		return b.iterationCount - a.iterationCount;
 	});
 	document.getElementById('td1').innerHTML = leaderBoard[0].id;
 	document.getElementById('td2').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td3').innerHTML = leaderBoard[0].id;
-	document.getElementById('td4').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td5').innerHTML = leaderBoard[0].id;
-	document.getElementById('td6').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td7').innerHTML = leaderBoard[0].id;
-	document.getElementById('td8').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td9').innerHTML = leaderBoard[0].id;
-	document.getElementById('td10').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td11').innerHTML = leaderBoard[0].id;
-	document.getElementById('td12').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td13').innerHTML = leaderBoard[0].id;
-	document.getElementById('td14').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td15').innerHTML = leaderBoard[0].id;
-	document.getElementById('td16').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td17').innerHTML = leaderBoard[0].id;
-	document.getElementById('td18').innerHTML = leaderBoard[0].iterationCount;
-	document.getElementById('td19').innerHTML = leaderBoard[0].id;
-	document.getElementById('td20').innerHTML = leaderBoard[0].iterationCount;
+	document.getElementById('td3').innerHTML = leaderBoard[0].name;
+
+	document.getElementById('td4').innerHTML = leaderBoard[1].id;
+	document.getElementById('td5').innerHTML = leaderBoard[1].iterationCount;
+	document.getElementById('td6').innerHTML = leaderBoard[1].name;
+
+	document.getElementById('td7').innerHTML = leaderBoard[2].id;
+	document.getElementById('td8').innerHTML = leaderBoard[2].iterationCount;
+	document.getElementById('td9').innerHTML = leaderBoard[2].name;
+
+	document.getElementById('td10').innerHTML = leaderBoard[3].id;
+	document.getElementById('td11').innerHTML = leaderBoard[3].iterationCount;
+	document.getElementById('td12').innerHTML = leaderBoard[3].name;
+
+	document.getElementById('td13').innerHTML = leaderBoard[4].id;
+	document.getElementById('td14').innerHTML = leaderBoard[4].iterationCount;
+	document.getElementById('td15').innerHTML = leaderBoard[4].name;
+
+	document.getElementById('td16').innerHTML = leaderBoard[5].id;
+	document.getElementById('td17').innerHTML = leaderBoard[5].iterationCount;
+	document.getElementById('td18').innerHTML = leaderBoard[5].name;
+
+	document.getElementById('td19').innerHTML = leaderBoard[6].id;
+	document.getElementById('td20').innerHTML = leaderBoard[6].iterationCount;
+	document.getElementById('td21').innerHTML = leaderBoard[6].name;
+
+	document.getElementById('td22').innerHTML = leaderBoard[7].id;
+	document.getElementById('td23').innerHTML = leaderBoard[7].iterationCount;
+	document.getElementById('td24').innerHTML = leaderBoard[7].name;
+
+	document.getElementById('td25').innerHTML = leaderBoard[8].id;
+	document.getElementById('td26').innerHTML = leaderBoard[8].iterationCount;
+	document.getElementById('td27').innerHTML = leaderBoard[8].name;
+
+	document.getElementById('td28').innerHTML = leaderBoard[9].id;
+	document.getElementById('td29').innerHTML = leaderBoard[9].iterationCount;
+	document.getElementById('td30').innerHTML = leaderBoard[9].name;
+
 	return leaderBoard.slice(0, 10);
 };
